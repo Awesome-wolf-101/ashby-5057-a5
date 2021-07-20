98911fa415d98bfc765e135da5f8d18b20cbd5e7
Joshua Ashby <joshuaashby16@gmail.com>
2021-07-20 11:48:27 -0400

    Added the ability to sort the items of the list.

--- src/main/java/ucf/assignments/InventoryManagerController.java
+++ src/main/java/ucf/assignments/InventoryManagerController.java
@@ -93,7 +93,6 @@ public class InventoryManagerController implements Initializable {
         {
             SortListByName(items);
         }
-
         InventoryManagerTableView.getItems().setAll(items);
     }
 
